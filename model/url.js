aedcb2c9ab3a86939c96ac7c8c2bfe3fd3b08b5a
djoun52 <simon.rioux.trv@gmail.com>
2021-09-21 16:13:52 +0200

    création du model et implémentation dns la bdd

---
+++ model/url.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+    original : {type : String, required: true}, 
+    short : Number  
+})
+
+let url = mongoose.model("url", urlSchema);
+
+
+exports.UrlModel = url
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/controller", express.static(__dirname + '/controller'))
+app.use("/model", express.static(__dirname + '/model'))
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const url = require("./model/url.js").UrlModel
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+// app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +28,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+let responceObject = {}
+app.post("/api/shorturl", bodyParser.urlencoded({extended: false}) , (req, res)=>{
+  let inputUrl = req.body['url']
+  responceObject['original_url'] = inputUrl
+  let inputShort = 1
+
+  url.findOne({})
+      .sort({sort: 'desc'})
+      .exec( (err, data)=>{
+        if (!err && data != undefined){
+          inputShort = data.short +1 
+        }
+        if(!err){
+          url.findOneAndUpdate(
+            {original: inputUrl},
+            {original: inputUrl, short: inputShort},
+            {new: true, upsert: true}, (err , saveUrl) =>{
+              if (err) {
+                return console.error(err);
+              }else{
+                responceObject["short_url"] = saveUrl.short;
+                res.json(responceObject);
+              }
+            }
+          )
+        }
+      })
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
